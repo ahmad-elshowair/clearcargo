1e1bae35591ec17e41c0ff51cd9026906cfce76d
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-09-10 20:46:26 +0700

    add initial login and register pages

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+	return (
+		<section className="lg:min-h-screen xl:min-h-screen h-[calc(100vh-168px)] p-4 w-full">
+			<section className="grid gap-6 rounded-lg bg-gray-300/35 shadow h-full justify-items-stretch p-5 border border-green-400/50">
+				<h1>Login</h1>
+			</section>
+		</section>
+	);
+};
+
+export default LoginPage;
