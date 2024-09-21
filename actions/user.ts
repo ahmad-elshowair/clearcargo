b26b89d424559186632f708f81235e2c98b3e034
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-09-21 22:36:35 +0700

    edit get user function to use to the custom function on supabase of getting user by email,  create a new server function to fetch all users.

--- actions/user.ts
+++ actions/user.ts
@@ -1,24 +1,72 @@
 "use server";
 import { createAdminClient } from "@/lib/supabase/admin";
+import { User } from "@supabase/supabase-js";
+const supabaseAdmin = createAdminClient();
+
+// FETCH ALL USERS
+export const fetchAllUser = async () => {
+	try {
+		let users: User[] = [];
+		let page: number | undefined = undefined;
+
+		do {
+			const { data, error } = await supabaseAdmin.auth.admin.listUsers({
+				page,
+			});
+			if (error) {
+				console.error(`SUPABASE FETCHING ALL USERS: ${error.message}`);
+				return {
+					status: "error",
+					message: error.message,
+					data: null,
+				};
+			}
+
+			if (data && data.users) {
+				users = users.concat(data.users);
+				page = data.nextPage ?? undefined;
+			} else {
+				page = undefined;
+			}
+		} while (page);
+		return {
+			status: "success",
+			message: "USERS FETCHED SUCCESSFULLY",
+			data: users,
+		};
+	} catch (error) {
+		console.error(`ERROR FETCHING ALL USERS: ${(error as Error).message}`);
+		return {
+			status: "error",
+			message: (error as Error).message,
+			data: null,
+		};
+	}
+};
+
+// FETCH A USER BY EMAIL.
 
 export const getUserByEmail = async (email: string) => {
-	const supabaseAdmin = createAdminClient();
+	try {
+		const { data, error } = await supabaseAdmin
+			.rpc("get_user_by_email", {
+				p_email: email,
+			})
+			.maybeSingle();
 
-	// Fetch all users and manually filter by email
-	const { data, error } = await supabaseAdmin.auth.admin.listUsers();
+		if (error) {
+			console.error("ERROR WITH SUPABASE ADMIN:", error.message);
+			return { status: "error", message: error.message, data: null };
+		}
 
-	if (error) {
-		return { status: "error", message: error.message, data: null };
+		if (data) {
+			console.log("the user: ", data);
+			return { status: "success", message: "USER FOUND", data: data };
+		} else {
+			return { status: "error", message: "USER NOT FOUND", data: null };
+		}
+	} catch (error) {
+		console.error("ERROR FETCHING USER BY EMAIL: ", (error as Error).message);
+		return { status: "error", message: (error as Error).message, data: null };
 	}
-
-	// Manually filter the users array to find the user with the matching email
-	const user = data.users.find((user) => user.email === email);
-
-	console.log("the user: ", user);
-
-	if (user) {
-		return { status: "success", message: "Found user", data: user };
-	}
-
-	return { status: "error", message: "User not found", data: null };
 };
