589819650180a2723528d2696bac9ba5fdf63544
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-10-03 20:40:05 +0700

    add validations of file size and types in the schema of creating a clearance.

--- lib/schemas/clearance.ts
+++ lib/schemas/clearance.ts
@@ -1,13 +1,29 @@
 import { z } from "zod";
 
+const MAX_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_TYPES = [
+	"image/jpeg",
+	"image/png",
+	"image/jpg",
+	"application/pdf",
+];
+
+const fileSchema = z
+	.custom<File>((file) => file instanceof File, "Invalid file type.")
+	.refine((file) => file.size <= MAX_SIZE, "File size exceeds 5MB.")
+	.refine(
+		(file) => ACCEPTED_TYPES.includes(file.type),
+		"only .jpeg, .jpg, .png, and .pdf files are allowed.",
+	);
+
 export const CreateClearanceSchema = z
 	.object({
 		port_id: z.string().uuid(),
 		is_vat_paid: z.boolean(),
-		vat_receipt: z.any().nullable().optional(),
+		vat_receipt: z.union([fileSchema, z.null()]).optional(),
 		arrival_date: z.date(),
-		invoice: z.union([z.instanceof(File), z.string().url()]).nullable(),
-		loading_bill: z.union([z.instanceof(File), z.string().url()]).nullable(),
+		invoice: z.union([fileSchema, z.string().url(), z.null()]),
+		loading_bill: z.union([fileSchema, z.string().url(), z.null()]),
 	})
 	.refine(
 		(data) => {
