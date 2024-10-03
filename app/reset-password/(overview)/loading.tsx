9563f0855f0aa29ed51b4ca217fc532c8234dfd4
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-10-04 06:16:39 +0700

    style the loading components

--- app/reset-password/(overview)/loading.tsx
+++ app/reset-password/(overview)/loading.tsx
@@ -1,7 +1,9 @@
 const ResetPasswordLoading = () => {
 	return (
 		<section className="lg:min-h-screen xl:min-h-screen h-[calc(100vh-168px)] p-4 w-full">
-			loading...
+			<div className="grid gap-6 rounded-lg bg-gray-300/35 shadow h-full justify-items-stretch p-5 border border-green-400/50">
+				loading...
+			</div>
 		</section>
 	);
 };
