3a08173f0acdc92059acb0dd9f31e3348039c6ca
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-10-08 21:13:13 +0700

    add delete file function

---
+++ lib/supabase/deleteFile.ts
@@ -0,0 +1,40 @@
+import { createSupabaseAdminClient } from "./admin";
+
+export const deleteFile = async (
+	fileUrl: string,
+	folder: "invoices" | "loading_bills" | "vat_receipts",
+) => {
+	try {
+		const supabase = createSupabaseAdminClient();
+		const fullPath = `${folder}/${fileUrl}`;
+
+		const { data, error } = await supabase.storage
+			.from("clearcargo")
+			.remove([fullPath]);
+		if (error) {
+			console.error("ERROR DELETING FILE: ", error);
+			return {
+				status: "error",
+				message: error.message,
+			};
+		}
+
+		if (data && data.length > 0) {
+			return {
+				status: "success",
+				message: "File deleted successfully",
+			};
+		} else {
+			return {
+				status: "error",
+				message: "File not found",
+			};
+		}
+	} catch (error) {
+		console.error("ERROR DELETING FILE: ", error);
+		return {
+			status: "error",
+			message: (error as Error).message,
+		};
+	}
+};
