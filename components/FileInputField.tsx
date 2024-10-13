a0a8d71f472e18db6365cfd47948796ab8b8b52d
Ahmad Elshowair <ahmad-elshowair.dev@outlook.com>
2024-10-13 20:24:36 +0700

    show an icon to show the current file if there is file fetched.

--- components/FileInputField.tsx
+++ components/FileInputField.tsx
@@ -6,7 +6,9 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 import { FieldPath, FieldValues, UseFormReturn } from "react-hook-form";
+import { LuScreenShare } from "react-icons/lu";
 
 const ACCEPTED_FILE_TYPES = [
 	"image/jpeg",
@@ -26,6 +28,7 @@ export function FileInputField<T extends FieldValues>({
 	label,
 	form,
 }: FileInputFieldProps<T>) {
+	const currentFile = form.watch(name);
 	return (
 		<FormField
 			control={form.control}
@@ -36,15 +39,26 @@ export function FileInputField<T extends FieldValues>({
 						{label}
 					</FormLabel>
 					<FormControl>
-						<Input
-							type="file"
-							className="bg-white"
-							accept={ACCEPTED_FILE_TYPES.join(",")}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-								const file = e.target.files?.[0] || null;
-								field.onChange(file);
-							}}
-						/>
+						<div className="relative flex items-center gap-1">
+							{currentFile && typeof currentFile === "string" && (
+								<Link
+									href={currentFile}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-green-500 hover:text-green-700 rounded-md bg-white py-1 px-2">
+									<LuScreenShare className="pointer-events-none w-6 h-6" />
+								</Link>
+							)}
+							<Input
+								type="file"
+								className="bg-white"
+								accept={ACCEPTED_FILE_TYPES.join(",")}
+								onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+									const file = e.target.files?.[0] || null;
+									field.onChange(file);
+								}}
+							/>
+						</div>
 					</FormControl>
 					<FormMessage />
 				</FormItem>
